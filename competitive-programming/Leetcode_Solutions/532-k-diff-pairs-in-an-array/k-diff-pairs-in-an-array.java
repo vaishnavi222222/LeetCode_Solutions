7aca81f017de0cf935d6204547e46f2be6983469
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-04-17 14:45:36 +0530

    Time: 427 ms (5.09%) | Memory: 44.6 MB (88.62%) - LeetSync

---
+++ competitive-programming/Leetcode_Solutions/532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        HashSet<String> hs = new HashSet<>();
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k && i != j) {
+                    int a = Math.min(nums[i], nums[j]);
+                    int b = Math.max(nums[i], nums[j]);
+                    String pair = a + "," + b;
+                    if (!hs.contains(pair)) {
+                        hs.add(pair);
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+}
