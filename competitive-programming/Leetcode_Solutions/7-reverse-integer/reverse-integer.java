b77762ed2b007defe61cb980a082b786e577ac01
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-04-22 21:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (85.31%) - LeetSync

--- competitive-programming/Leetcode_Solutions/7-reverse-integer/reverse-integer.java
+++ competitive-programming/Leetcode_Solutions/7-reverse-integer/reverse-integer.java
@@ -1,14 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        long rev = 0;
+        long ans = 0;
         while (x != 0) {
             int rem = x % 10;
-            rev = rev * 10 + rem;
+            ans = ans * 10 + rem;
             x /= 10;
         }
-        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+        if (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) {
             return 0;
         }
-        return (int) rev;
+        return (int) ans;
     }
 }
