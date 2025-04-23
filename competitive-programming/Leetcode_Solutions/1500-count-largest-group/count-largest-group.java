538a58d3a709cc76ea7e466edcc236ce394fcee4
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-04-23 21:07:29 +0530

    Time: 10 ms (45.52%) | Memory: 41.5 MB (23.66%) - LeetSync

---
+++ competitive-programming/Leetcode_Solutions/1500-count-largest-group/count-largest-group.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countLargestGroup(int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxSize = 0;
+        for (int i =1; i<= n; i++) {
+            int sum = 0, num = i;
+            while (num > 0) {
+                sum +=num % 10;
+                num /= 10;
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            maxSize = Math.max(maxSize, map.get(sum));
+        }
+        int cnt = 0;
+        for (int val : map.values()) {
+            if (val == maxSize) cnt++;
+        }
+        return cnt;
+    }
+}
