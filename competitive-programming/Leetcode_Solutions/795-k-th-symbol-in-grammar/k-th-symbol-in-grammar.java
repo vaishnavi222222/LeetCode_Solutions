32ea5e3e38d31c86b839174ea1a3d787d736279d
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-08-08 11:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (49.83%) - LeetSync

---
+++ competitive-programming/Leetcode_Solutions/795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(k == 1 ) return 0;
+        int mid = ((int)Math.pow(2,n-1) /2);
+        if(k <= mid )return kthGrammar(n-1,k);
+        else return 1 - kthGrammar(n-1,k-mid);
+    }
+}
