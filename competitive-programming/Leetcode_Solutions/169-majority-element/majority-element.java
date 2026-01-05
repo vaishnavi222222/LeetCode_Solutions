16584612a48755748ecafb9ca32dc5b217a5963b
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2026-01-05 18:25:13 +0530

    Time: 2 ms (73.52%) | Memory: 55.9 MB (7.01%) - LeetSync

--- competitive-programming/Leetcode_Solutions/169-majority-element/majority-element.java
+++ competitive-programming/Leetcode_Solutions/169-majority-element/majority-element.java
@@ -1,6 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // moree voting
+        int count = 0;
+        int candidate = 0;
+        for(int i=0;i<nums.length;i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            if(nums[i] == candidate) count ++;
+            else count --;
+        }
+        return candidate;
     }
 }
