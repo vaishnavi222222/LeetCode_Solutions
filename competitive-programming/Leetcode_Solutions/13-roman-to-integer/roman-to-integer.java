ea90fdbe77a25bc0ba01c0e0867c3f86b2af04cd
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2026-01-09 12:18:05 +0530

    Time: 5 ms (34.62%) | Memory: 47 MB (11.62%) - LeetSync

--- competitive-programming/Leetcode_Solutions/13-roman-to-integer/roman-to-integer.java
+++ competitive-programming/Leetcode_Solutions/13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        int ans = 0, num = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
-            }
-            if (4 * num < ans)
-                ans -= num;
-            else
-                ans += num;
+    HashMap<Character,Integer> map = new HashMap<>();
+    map.put('I',1);
+    map.put('V',5);
+    map.put('X',10);
+    map.put('L',50);
+    map.put('C',100);
+    map.put('D',500);
+    map.put('M',1000);
+    int i=0;
+    int num = 0;
+    while (i<s.length()){
+        if(i < s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+            num += map.get(s.charAt(i+1)) - map.get(s.charAt(i));
+            i++;
         }
-        return ans;
+        else{
+            num += map.get(s.charAt(i));
+        }
+        i++;
+    }
+        return num;
     }
 }
